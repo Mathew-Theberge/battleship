5b6ac6e97afe84e63636bfc5a09ca1db1744e13a
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-03-09 18:47:53 -0400

    Setup work environment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintPluginPrettier, { languageOptions } from "eslint-plugin-prettier/recommended";
 
 export default [
     { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    {languageOptions: {globals: globals.jest}},
     pluginJs.configs.recommended,
     eslintPluginPrettier,
 ];
