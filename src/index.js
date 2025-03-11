c578e4c761c35256b995ebed151ec0ce4bd1e076
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-03-11 01:49:46 -0400

    use gamecontroller logic to create basic back and forth gameplay

--- src/index.js
+++ src/index.js
@@ -6,37 +6,61 @@ import {
     renderGameboard,
     renderShips,
 } from "./DOM.js";
+import { gameController } from "./gameController.js";
 
-const P1 = createGameboard("P1");
-const P2 = createGameboard("P2");
+const playComputerBtn = document.querySelector("#playComputer");
 
-renderGameboard(P1, "P1");
-renderGameboard(P2, "P2");
+playComputerBtn.addEventListener(
+    "click",
+    () => {
+        const P1 = createGameboard("P1");
+        const P2 = createGameboard("P2");
 
-const player = new Player("player");
-const computer = new Player("computer");
+        renderGameboard(P1, "P1");
+        renderGameboard(P2, "P2");
 
-player.gameboard.placeShip("a", 1, "hor", 5);
-player.gameboard.placeShip("b", 7, "ver", 4);
-player.gameboard.placeShip("j", 5, "hor", 3);
-player.gameboard.placeShip("c", 10, "ver", 3);
-player.gameboard.placeShip("a", 7, "hor", 2);
+        const player = new Player("player");
+        const computer = new Player("computer");
 
-computer.gameboard.placeShip("a", 5, "hor", 5);
-computer.gameboard.placeShip("f", 7, "ver", 4);
-computer.gameboard.placeShip("j", 2, "hor", 3);
-computer.gameboard.placeShip("c", 4, "ver", 3);
-computer.gameboard.placeShip("b", 2, "hor", 2);
+        player.gameboard.placeShip("a", 1, "hor", 5);
+        player.gameboard.placeShip("b", 7, "ver", 4);
+        player.gameboard.placeShip("j", 5, "hor", 3);
+        player.gameboard.placeShip("c", 10, "ver", 3);
+        player.gameboard.placeShip("a", 7, "hor", 2);
 
-renderShips(player.gameboard.board, "P1");
-renderShips(computer.gameboard.board, "P2");
+        computer.gameboard.placeShip("a", 5, "hor", 5);
+        computer.gameboard.placeShip("f", 7, "ver", 4);
+        computer.gameboard.placeShip("j", 2, "hor", 3);
+        computer.gameboard.placeShip("c", 4, "ver", 3);
+        computer.gameboard.placeShip("b", 2, "hor", 2);
 
-player.gameboard.receiveAttack("b", 4);
-player.gameboard.receiveAttack("b", 3);
-player.gameboard.receiveAttack("b", 7);
-renderAttacks(player.gameboard.board, "P1");
+        renderShips(player.gameboard.board, "P1");
+        console.log(player.gameboard.allAttacks);
 
-computer.gameboard.receiveAttack("b", 4);
-computer.gameboard.receiveAttack("b", 3);
-computer.gameboard.receiveAttack("b", 7);
-renderAttacks(computer.gameboard.board, "P2");
+        const enemyTiles = document.querySelectorAll(".P2");
+
+        enemyTiles.forEach((tile) => {
+            let char = tile.getAttribute("class")[0].toLowerCase();
+            let num = tile.getAttribute("class").slice(1, 3);
+            if (num.includes("-")) {
+                num = num.slice(0, 1);
+            }
+            tile.addEventListener("click", () => {
+                if (gameController.player1sTurn) {
+                    const attack = computer.gameboard.receiveAttack(char, +num);
+                    renderAttacks(computer.gameboard.board, "P2");
+                    if (attack === "already been attacked") {
+                        alert("already been attacked");
+                    } else {
+                        gameController.changePlayerTurn();
+                        gameController.playComputersTurn(
+                            computer.gameboard,
+                            player.gameboard,
+                        );
+                    }
+                }
+            });
+        });
+    },
+    { once: true },
+);
