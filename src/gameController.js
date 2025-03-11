39d200914967518ef421cd959b9903691e3e727c
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2025-03-11 03:47:17 -0400

    Refactor code so less loose logic is in eventlisteners
    extended funcinality to gameController

--- src/gameController.js
+++ src/gameController.js
@@ -1,8 +1,45 @@
-import { renderAttacks } from "./DOM";
+import {
+    createGameboard,
+    renderAttacks,
+    renderGameboard,
+    renderPlayComputerUI,
+    renderShips,
+    renderAttackLog,
+} from "./DOM.js";
+
+import { Player, receiveAttack } from "./app.js";
 
 export const gameController = {
     player1sTurn: true,
     player2sTurn: false,
+    player1: null,
+    player2: null,
+
+    startComputerGame: () => {
+        renderPlayComputerUI();
+        const P1 = createGameboard("P1");
+        const P2 = createGameboard("P2");
+
+        renderGameboard(P1, "P1");
+        renderGameboard(P2, "P2");
+
+        gameController.player1 = new Player("player");
+        gameController.player2 = new Player("computer");
+
+        gameController.player1.gameboard.placeShip("a", 1, "hor", 5);
+        gameController.player1.gameboard.placeShip("b", 7, "ver", 4);
+        gameController.player1.gameboard.placeShip("j", 5, "hor", 3);
+        gameController.player1.gameboard.placeShip("c", 10, "ver", 3);
+        gameController.player1.gameboard.placeShip("a", 7, "hor", 2);
+
+        gameController.player2.gameboard.placeShip("a", 5, "hor", 5);
+        gameController.player2.gameboard.placeShip("f", 7, "ver", 4);
+        gameController.player2.gameboard.placeShip("j", 2, "hor", 3);
+        gameController.player2.gameboard.placeShip("c", 4, "ver", 3);
+        gameController.player2.gameboard.placeShip("b", 2, "hor", 2);
+
+        renderShips(gameController.player1.gameboard.board, "P1");
+    },
 
     changePlayerTurn: () => {
         if (gameController.player1sTurn) {
@@ -14,11 +51,12 @@ export const gameController = {
         }
     },
 
-    playComputersTurn: (playerBoard) => {
+    playComputersTurn: () => {
         let allAttacks = getAllAttacksArray();
-        for (const attack of playerBoard.allAttacks) {
+        for (const attack of gameController.player1.gameboard.allAttacks) {
             delete allAttacks[attack];
         }
+        // picks random attack cords from the remaining ones
         let attack =
             allAttacks[
                 Object.keys(allAttacks)[
@@ -27,9 +65,26 @@ export const gameController = {
             ];
         let char = attack[0];
         let num = attack.slice(1);
-        playerBoard.receiveAttack(char, num);
-        renderAttacks(playerBoard.board, "P1");
+        gameController.player1.gameboard.receiveAttack(char, num);
+        renderAttacks(gameController.player1.gameboard.board, "P1");
         gameController.changePlayerTurn();
+    },
+
+    playPlayersTurn: (char, num) => {
+        if (gameController.player1sTurn) {
+            const attack = gameController.player2.gameboard.receiveAttack(
+                char,
+                +num,
+            );
+            renderAttacks(gameController.player2.gameboard.board, "P2");
+            renderAttackLog(gameController.player2.gameboard);
+            if (attack === "already been attacked") {
+                alert("already been attacked");
+            } else {
+                gameController.changePlayerTurn();
+                gameController.playComputersTurn();
+            }
+        }
     },
 };
 
@@ -46,5 +101,3 @@ function getAllAttacksArray() {
     }
     return allAttacks;
 }
-
-console.log(getAllAttacksArray());
--- src/index.js
+++ src/index.js
@@ -1,64 +1,28 @@
 import "./style.css";
 import { Gameboard, Player, Ship } from "./app.js";
-import {
-    createGameboard,
-    renderAttacks,
-    renderGameboard,
-    renderShips,
-} from "./DOM.js";
+import { renderAttackLog, renderAttacks, renderStartScreenUI } from "./DOM.js";
 import { gameController } from "./gameController.js";
 
+renderStartScreenUI();
 const playComputerBtn = document.querySelector("#playComputer");
 
 playComputerBtn.addEventListener(
     "click",
     () => {
-        const P1 = createGameboard("P1");
-        const P2 = createGameboard("P2");
-
-        renderGameboard(P1, "P1");
-        renderGameboard(P2, "P2");
-
-        const player = new Player("player");
-        const computer = new Player("computer");
-
-        player.gameboard.placeShip("a", 1, "hor", 5);
-        player.gameboard.placeShip("b", 7, "ver", 4);
-        player.gameboard.placeShip("j", 5, "hor", 3);
-        player.gameboard.placeShip("c", 10, "ver", 3);
-        player.gameboard.placeShip("a", 7, "hor", 2);
-
-        computer.gameboard.placeShip("a", 5, "hor", 5);
-        computer.gameboard.placeShip("f", 7, "ver", 4);
-        computer.gameboard.placeShip("j", 2, "hor", 3);
-        computer.gameboard.placeShip("c", 4, "ver", 3);
-        computer.gameboard.placeShip("b", 2, "hor", 2);
-
-        renderShips(player.gameboard.board, "P1");
-        console.log(player.gameboard.allAttacks);
+        gameController.startComputerGame();
 
         const enemyTiles = document.querySelectorAll(".P2");
 
         enemyTiles.forEach((tile) => {
+            // grabbing cords from each tile to use in receiveAttack func
             let char = tile.getAttribute("class")[0].toLowerCase();
             let num = tile.getAttribute("class").slice(1, 3);
             if (num.includes("-")) {
                 num = num.slice(0, 1);
             }
+
             tile.addEventListener("click", () => {
-                if (gameController.player1sTurn) {
-                    const attack = computer.gameboard.receiveAttack(char, +num);
-                    renderAttacks(computer.gameboard.board, "P2");
-                    if (attack === "already been attacked") {
-                        alert("already been attacked");
-                    } else {
-                        gameController.changePlayerTurn();
-                        gameController.playComputersTurn(
-                            computer.gameboard,
-                            player.gameboard,
-                        );
-                    }
-                }
+                gameController.playPlayersTurn(char, num);
             });
         });
     },
